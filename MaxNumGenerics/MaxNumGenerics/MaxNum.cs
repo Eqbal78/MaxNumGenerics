9ec42de8fca8de26e074eca5942d959b5d983e1a
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-17 19:38:39 +0530

    Three Float Numbers find Maximum

--- MaxNumGenerics/MaxNumGenerics/MaxNum.cs
+++ MaxNumGenerics/MaxNumGenerics/MaxNum.cs
@@ -23,5 +23,22 @@ namespace MaxNumGenerics
             }
         }
 
+        public static float Max(float a, float b, float c)
+        {
+            if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
+            {
+                return a;
+
+            }
+            else if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+
     }
 }
--- MaxNumGenerics/MaxNumGenerics/Program.cs
+++ MaxNumGenerics/MaxNumGenerics/Program.cs
@@ -7,8 +7,13 @@ namespace MaxNumGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Generics Code!");
+
             int result=MaxNum.Max(22, 88, 66);
             Console.WriteLine("\nMaximum Integer Number is : {0}", result);
+            Console.WriteLine("----------------------------------------------");
+
+            float resultFloat = MaxNum.Max(12.2f, 1.8f, 9.6f);
+            Console.WriteLine("\nMaximum Float Number is : {0}", resultFloat);
         }
     }
 }
