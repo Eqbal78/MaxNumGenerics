a883a625af4b98044fb55b4007809bc8af7a8df1
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-17 19:25:52 +0530

    3 Integers find

---
+++ MaxNumGenerics/MaxNumGenerics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaxNumGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generics Code!");
+            int result=MaxNum.Max(22, 88, 66);
+            Console.WriteLine("Maximum Integer Number is : {0}", result);
+        }
+    }
+}
