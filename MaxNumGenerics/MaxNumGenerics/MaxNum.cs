7f827cb02dafd9614001c170d87a8ed62048e5fa
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-17 21:33:13 +0530

    Find Maximum using Generic Class

--- MaxNumGenerics/MaxNumGenerics/MaxNum.cs
+++ MaxNumGenerics/MaxNumGenerics/MaxNum.cs
@@ -4,9 +4,21 @@ using System.Text;
 
 namespace MaxNumGenerics
 {
-    class MaxNum
+    public class MaxNum<T> where T : IComparable
     {
-        public static T Max<T>(T a, T b, T c) where T : IComparable
+        // Instance variables
+        public T a, b, c;
+
+        // Constructor
+        public MaxNum(T a, T b, T c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+        }
+        // Generic method to find maximum value
+        public static T Max(T a, T b, T c)
         {
             if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
             {
@@ -21,6 +33,12 @@ namespace MaxNumGenerics
             {
                 return c;
             }
+        }
+        // Generic method to call maximum method
+        public T MaxMethod()
+        {
+            T maxNumber = MaxNum<T>.Max(this.a, this.b, this.c);
+            return maxNumber;
         }
 
     }
--- MaxNumGenerics/MaxNumGenerics/Program.cs
+++ MaxNumGenerics/MaxNumGenerics/Program.cs
@@ -7,18 +7,23 @@ namespace MaxNumGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Generics Code!");
-
-            int result=MaxNum.Max(22, 88, 66);
+            //Maximum Integer Number
+            MaxNum<int> obj = new MaxNum<int>(22, 88, 66);
+            int result=obj.MaxMethod();
             Console.WriteLine("\nMaximum Integer Number is : {0}", result);
 
             Console.WriteLine("----------------------------------------------");
 
-            float resultFloat = MaxNum.Max(12.2f, 1.8f, 9.6f);
+            //Maximum Float Number
+            MaxNum<float> obj1 = new MaxNum<float>(12.2f, 1.8f, 9.6f);
+            float resultFloat = obj1.MaxMethod();
             Console.WriteLine("\nMaximum Float Number is : {0}", resultFloat);
 
             Console.WriteLine("----------------------------------------------");
 
-            string resultString = MaxNum.Max("Apple", "Peach", "Mango");
+            //Maximum String Number
+            MaxNum<string> obj3 = new MaxNum<string>("Apple", "Peach", "Mango");
+            string resultString = obj3.MaxMethod();
             Console.WriteLine("\nMaximum Float Number is : {0}", resultString);
         }
     }
